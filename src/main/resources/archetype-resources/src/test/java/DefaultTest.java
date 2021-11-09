c94daa377091d29b548d9c8115049dec6c6cefe9
Daniel Romero Díaz(Parrot14) <parrot14.mail@gmail.com>
2021-11-09 02:17:27 -0600

    First stable and production ready version of (Simple Archetype) default java project template

---
+++ src/main/resources/archetype-resources/src/test/java/DefaultTest.java
@@ -0,0 +1,14 @@
+package $package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DefaultTest 
+{
+    @Test
+    public void trueTest()
+    {
+        assertTrue( true );
+    }
+}
